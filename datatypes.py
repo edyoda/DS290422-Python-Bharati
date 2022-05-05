db7e3b611dcc42c9735b63cdb0837698707d5ece
Bharati Yadav <bharati.yadav@engagely.ai>
2022-05-05 14:47:30 +0530

    datatypes

--- datatypes.py
+++ datatypes.py
@@ -15,35 +15,77 @@ float
 complex
 bool
 str
-bytes
-bytearray
-range
 list
 tuple
 set
-frozenset
 dict
 None
 """
 
-# Dynamically Typed
+
+# int datatype
 num = 20
 print(type(num))
 
+
+# float datatype
 num_1 = 56.7
 print(type(num_1))
 
+
+# str datatype
 name = "Edyoda"
 print(type(name))
 
+
+# bool datatype
 female = True
 print(type(female))
 
 
+# complex datatype
+complex = 3+2j
+print(type(complex))
 
 
+# list datatype
+# list - [] , mutable , allows duplicate elements
+student_names = ["Harsh","Omkar","Deepshikha","Nikunj"]
+print(student_names)                   # to fetch all the elements
+student_names[3] = "Shubham"           # to fetch the element on the bases of index
+print(student_names)
+print(type(student_names))
+print(student_names)
+print(student_names[3],student_names[0])
+
+# lst = [1,2,2,4,5,7,2,2,2,2,2,35,7,898,"1.Bharati","A",7.8]
+# print(lst)
 
 
+# tuple datatype
+# tuple - () , immutable, allows duplicate elements
+student_names = ("Harsh","Omkar","Deepshikha","Nikunj","Deepshikha")
+print(student_names)
+print(student_names[1])
 
 
+# set datatype
+# set - {} , mutable , non-indexed
+set_example = {1,2,5,6,5,5,5,1,7,8,9,9,6}
+print(set_example)
 
+
+# dict datatype
+# dict - {key:pair} , mutable, non-indexed, it stores data in key and value format
+names = {"Rajkumar":"Bharati",2:"Hritik",3:"Ram",4:"Bharati"}
+print(names[2])
+names[5] = "Meraj"
+print(names)
+names[3] = "Laxman"
+print(names)
+
+
+# None datatype
+name = None
+print(name)
+print(type(name))
--- type_conversion.py
+++ type_conversion.py
@@ -1,20 +1,52 @@
-# Type Conversion
+# Type Conversion (inbuilt functions)
 # 1. int()
 # 2. float()
 # 3. bool()
 # 4. str()
+# 5. complex()
+# 6. list()
+# 7. tuple()
+# 8. set()
+# 9. dict()
 
-num = 1
+num ="10.90" 
 print(type(num))
 
-num = str(num)
-print(type(num))
+int_conversion = int(num)
+print("Converted into int : ",type(int_conversion),int_conversion)
 
+float_conversion = float(num)
+print("Converted into float : ",type(float_conversion),float_conversion)
+
+str_conversion = str(num)
+print("Converted into str : ",type(str_conversion),str_conversion)
+
+bool_conversion = bool(num)
+print("Converted into bool : ",type(bool_conversion),bool_conversion)
+
+
+"""
+lst = [1,1,1,2,2,4,5,7,3,2,2,5]
+print(lst)
+
+# conversion of list (lst) to set to get only unique values
+lst1 = set(lst)
+print(lst1)
+
+# conversion of set to list to get the unique values in list
+lst2 = list(lst1)
+print(lst2)
+"""
+
+
+# num = 1
+# print(type(num))
+
+# converting to str
+# num = str(num)
+# print(type(num))
+
+# converting to bool
 # num = bool(num)
 # print(type(num))
 
-# num = 1
-# print(type(num)) #int
-
-# num = str(num)
-# print(type(num)) #str
